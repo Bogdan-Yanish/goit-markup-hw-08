414e3b8b1535eb29d12df9cf20b191f545fabb61
BOGDAN YANISHEVSKYY <byanishev@gmail.com>
2022-07-27 17:40:40 +0300

    Add mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,28 @@
+(() => {
+  const MenuBtnRef = document.querySelector("[data-menu-button]");
+  const MobileMenuRef = document.querySelector("[data-menu]");
+  const BodyRef = document.querySelector("body");
+  MenuBtnRef.addEventListener("click", () => {
+    const expanded =
+      MenuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    MenuBtnRef.classList.toggle("is-open");
+    MenuBtnRef.setAttribute("aria-expanded", !expanded);
+    MobileMenuRef.classList.toggle("is-open");
+    BodyRef.classList.toggle("is-open");
+  });
+})();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-open");
+//   }
+// })();
